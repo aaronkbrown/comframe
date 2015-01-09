fecf324c9afb25bbc7aeebaf30105d6af0dcbe88
Aaron Brown <aaron.kaoru.brown@gmail.com>
2015-01-08 21:37:07 -0800

    Popstate working in HTML5 history

--- public/main.js
+++ public/main.js
@@ -1,104 +1,98 @@
 
-// central element of page
-var content = document.getElementById("content");
 
-// function to get value of key (variable) in URL query string
-// Thank you, CSS-Tricks.com
-function getQueryVariable(variable) {
-  var query = window.location.search.substring(1);
-  var values = query.split("&");
-  for (var i = 0; i < values.length; i++){
-    var datapair = values[i].split("=");
-    if(datapair[0] == variable) {
-      return datapair[1];
+  // central element of page
+  var content = document.getElementById("content");
+
+  // function to get value of key (variable) in URL query string
+  // Thank you, CSS-Tricks.com
+  function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var values = query.split("&");
+    for (var i = 0; i < values.length; i++){
+      var datapair = values[i].split("=");
+      if(datapair[0] == variable) {
+        return datapair[1];
+      }
+    }
+    return(false);
+  }
+
+  // Get current page value of URL query string
+  var pageNumber = getQueryVariable("page");
+
+
+  // Display content of page determined by page number in query string
+  // If there is no current page number, print most current page
+  function printPage(pageVar) {
+    if(pageVar === false){
+      // print latest page
+      content.innerHTML = "<object type='text/html' data='content/current.html'></object>";
+    } else {
+      // get a string value of html file we want
+      var url = "content/" + pageVar + ".html";
+      // AJAX get request takes content file in question and prints to page
+      // Prints error page in case requested page does not exist
+      $.get(url).done(function(){
+        content.innerHTML = "<object type='text/html' data=" + url + "></object>";
+      }).fail(function(){
+        // If the page does not exist, redirect to root and remove from browser history
+        var navTo = window.location.href;
+        navTo = navTo.replace(window.location.search, "");
+        window.location.replace(navTo);
+      });
     }
   }
-  return(false);
-}
 
-// Get current page value of URL query string
-var pageNumber = getQueryVariable("page");
-
-
-// Display content of page determined by page number in query string
-// If there is no current page number, print most current page
-function printPage(pageVar) {
-  if(pageVar === false){
-    // print latest page
-    content.innerHTML = "<object type='text/html' data='content/current.html'></object>";
-    //content.setAttribute("data", "content/current.html");
-  } else {
-    // get a string value of html file we want
-    var url = "content/" + pageVar + ".html";
-    // string value of error page
-    //var errorURL = "content/error.html";
-    // AJAX get request takes content file in question and prints to page
-    // Prints error page in case requested page does not exist
-    $.get(url).done(function(){
-      content.innerHTML = "<object type='text/html' data=" + url + "></object>";
-    }).fail(function(){
-      content.innerHTML = "<object type='text/html' data='content/current.html'></object>";
-    });
-
-    /**
-    var url = "content/" + pageVar + ".html";
-    content.innerHTML = "<object type='text/html' data=" + url + "></object>";
-    //content.setAttribute("data", url);
-    */
+  function nextPage(){
+    pageNumber = getQueryVariable("page");
+    var pageInt = parseInt(pageNumber) + 1;
+    // Get current URL
+    var navTo = window.location.href;
+    // Truncate query string from URL
+    navTo = navTo.replace(window.location.search, "");
+    navTo = navTo + "?page=" + pageInt;
+    if(Modernizr.history){
+      // Use HTML5 history magic
+      history.pushState(null, null, navTo);
+      printPage(pageInt);
+    } else {
+      // Use older transition method as fallback
+      window.location.assign(navTo);
+    }
   }
-}
 
-function nextPage(){
-  pageNumber = getQueryVariable("page");
-  var pageInt = parseInt(pageNumber) + 1;
-  // Get current URL
-  var navTo = window.location.href;
-  // Truncate query string from URL
-  navTo = navTo.replace(window.location.search, "");
-  navTo = navTo + "?page=" + pageInt;
-  if(Modernizr.history){
-    // Use HTML5 history magic
-  } else {
-    // Use older transition method
-    window.location.assign(navTo);
+  function prevPage(){
+    pageNumber = getQueryVariable("page");
+    var pageInt = parseInt(pageNumber);
+    if(pageInt - 1 > 0){
+      pageInt = pageInt - 1;
+    }
+    var navTo = window.location.href;
+    navTo = navTo.replace(window.location.search, "");
+    navTo = navTo + "?page=" + pageInt;
+    if(Modernizr.history){
+      // Use HTML5 history magic
+      history.pushState(null, null, navTo);
+      printPage(pageInt);
+    } else {
+      // Use older transition method as fallback
+      window.location.assign(navTo);
+    }
   }
-  /**
-  printPage(parseInt(pageNumber) + 1);
-  */
-}
 
-function prevPage(){
-  pageNumber = getQueryVariable("page");
-  var pageInt = parseInt(pageNumber);
-  if(pageInt - 1 > 0){
-    pageInt = pageInt - 1;
-  }
-  var navTo = window.location.href;
-  navTo = navTo.replace(window.location.search, "");
-  navTo = navTo + "?page=" + pageInt;
-  if(Modernizr.history){
-    // Use HTML5 history magic
-  } else {
-    // Use older transition method
-    window.location.assign(navTo);
-  }
-  /**
-  if(pageNumber - 1 > 0){
-    printPage(pageNumber - 1);
-  } else {
-    printPage(1);
-  }
-  */
-}
+  printPage(pageNumber);
 
 
-printPage(pageNumber);
+  $("#prev").click(function(){
+    prevPage();
+  });
 
+  $("#next").click(function(){
+    nextPage();
+  });
 
-$("#prev").click(function(){
-  prevPage();
-});
+  // Popstate event listener for back button functionality in HTML5 History API
+  window.addEventListener("popstate", function(e){
+    printPage(getQueryVariable("page"));
+  });
 
-$("#next").click(function(){
-  nextPage();
-});
