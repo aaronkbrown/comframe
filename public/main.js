26ddf324b1a40ba97de9865fb78d142f1356d22b
Aaron Brown <aaron.kaoru.brown@gmail.com>
2015-01-08 19:08:14 -0800

    add modernizr.js, update main.js

--- public/main.js
+++ public/main.js
@@ -23,17 +23,69 @@ var pageNumber = getQueryVariable("page");
 // Display content of page determined by page number in query string
 // If there is no current page number, print most current page
 function printPage(pageVar) {
-  if(pageNumber === false){
+  if(pageVar === false){
     // print latest page
     content.innerHTML = "<object type='text/html' data='content/current.html'></object>";
     //content.setAttribute("data", "content/current.html");
   } else {
     // get a string value of html file we want
     var url = "content/" + pageVar + ".html";
+    // string value of error page
+    //var errorURL = "content/error.html";
+    // AJAX get request takes content file in question and prints to page
+    // Prints error page in case requested page does not exist
+    $.get(url).done(function(){
+      content.innerHTML = "<object type='text/html' data=" + url + "></object>";
+    }).fail(function(){
+      content.innerHTML = "<object type='text/html' data='content/current.html'></object>";
+    });
+
+    /**
+    var url = "content/" + pageVar + ".html";
     content.innerHTML = "<object type='text/html' data=" + url + "></object>";
     //content.setAttribute("data", url);
+    */
   }
 }
 
+function nextPage(){
+  pageNumber = getQueryVariable("page");
+
+  if(Modernizr.history){
+    // Use HTML5 history magic
+  } else {
+    // Use older transition method
+  }
+  /**
+  printPage(parseInt(pageNumber) + 1);
+  */
+}
+
+function prevPage(){
+  pageNumber = getQueryVariable("page");
+
+  if(Modernizr.history){
+    // Use HTML5 history magic
+  } else {
+    // Use older transition method
+  }
+  /**
+  if(pageNumber - 1 > 0){
+    printPage(pageNumber - 1);
+  } else {
+    printPage(1);
+  }
+  */
+}
+
+
 printPage(pageNumber);
 
+
+$("#prev").click(function(){
+  prevPage();
+});
+
+$("#next").click(function(){
+  nextPage();
+});
